e02a617193473e77d2c43e9731d19300675197ba
Guilherme Morgado <grmorgado@gmail.com>
2021-11-29 22:03:37 +0100

    multile tests in a file

--- tests/test_double.py
+++ tests/test_double.py
@@ -1,3 +1,8 @@
 def test_double(client, x=45):
     response = client.get(f"/double/{x}")
     assert response.data == b"<div>your result is:</div>90"
+
+
+def test_double00(client, x=0):
+    response = client.get(f"/double/{x}")
+    assert response.data == b"<div>your result is:</div>0"
